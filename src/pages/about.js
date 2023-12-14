b1f0450c350e584ee7248a4772baafebf22ee235
Eder Sena <eder.sena@live.com>
2023-12-14 11:45:16 -0300

    Fix typo in About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -62,7 +62,7 @@ const About = () => {
                 Biografia
               </h2>
               <p className="font-medium text-dark dark:text-light">
-                - Hello there! Im Eder, a skilled web developer driven by a love for crafting visually stunning, practical, and user-focused digital journeys. Armed with four years of hands-on experience in the field, Im constantly exploring fresh and inventive approaches to transform my clients' ideas into vibrant realities.
+                - Hello there! Im Eder, a skilled web developer driven by a love for crafting visually stunning, practical, and user-focused digital journeys. Armed with four years of hands-on experience in the field, Im constantly exploring fresh and inventive approaches to transform my clients ideas into vibrant realities.
               </p>
               <p className="my-4 font-medium text-dark dark:text-light">
                 - I hold the belief that design extends beyond mere aesthetics; its a problem-solving endeavor aimed at crafting intuitive and delightful user experiences.
